21c85770c66382b1247940ca5aff927bb34fb414
omar-nasri0 <zorokon82@gmail.com>
2024-12-01 19:12:53 +0200

    edit hight

--- src/components/Pages/Auth/auth.tsx
+++ src/components/Pages/Auth/auth.tsx
@@ -3,7 +3,7 @@ import bg from '/public/auth-bg.png'
 
 function auth() {
   return (
-    <div className="auth" style={{backgroundImage:`url(${bg})` , height:'150vh',backgroundPosition:'left'}}>
+    <div className="auth" style={{backgroundImage:`url(${bg})` , height:'100vh',backgroundPosition:'left'}}>
         <Outlet/>
       
     </div>
